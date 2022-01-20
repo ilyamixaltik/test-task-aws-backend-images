a542232d6adf5982f52781f99eccf69b9f39b2c7
ilyamixaltik <mixaltik16@gmail.com>
2022-01-20 13:57:27 +0300

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,25 @@
+const { PORT, MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
 type Server = {
   port: number
 }
 
+type MySQL = {
+  host: string,
+  port: number,
+  user: string,
+  password: string,
+  database: string
+}
+
 export const server: Server = {
-  port: Number(process.env.PORT) || 3000
+  port: Number(PORT) || 3000
+}
+
+export const mySQL: MySQL = {
+  host: MYSQL_HOST || 'localhost',
+  port: Number(MYSQL_PORT) || 3306,
+  user: MYSQL_USER || 'root',
+  password: MYSQL_PASSWORD || 'password',
+  database: MYSQL_DATABASE || 'test'
 }
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { server } from './config';
-
-const app = express();
-
-app.listen(server.port, (): void => {
-  console.log(`[Express] Listen on ${server.port} port`);
-});
